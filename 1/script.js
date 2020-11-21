eff212f202c301d6de04c0f81177168a0e3de2e4
Dmitriy Diukov <diukovdmitriy@gmail.com>
2020-11-21 17:53:35 +0200

    class task done

--- 1/script.js
+++ 1/script.js
@@ -1,8 +1,18 @@
 let randomNumber = Math.floor(Math.random() * (10 - 1) + 1);
-let guessNumber;
+// let guessNumber;
 
-do {
+// постусловие
+// do {
+//   guessNumber = prompt('Введи число от 1 до 10');
+// } while(guessNumber !== null && Number(guessNumber) !== randomNumber);
+
+// предусловие
+// while(guessNumber !== null && Number(guessNumber) !== randomNumber) {
+//   guessNumber = prompt('Введи число от 1 до 10');
+// }
+
+for(let guessNumber; guessNumber !== null && Number(guessNumber) !== randomNumber;) {
   guessNumber = prompt('Введи число от 1 до 10');
-} while(guessNumber !== null && Number(guessNumber) !== randomNumber);
+}
 
 alert('Вы угадали, случайное число - ' + randomNumber);
